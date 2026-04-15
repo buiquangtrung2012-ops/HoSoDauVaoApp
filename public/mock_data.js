a947f56526ff787dfc4e94cf14ba37998855c7f4
Bùi Quang Trung <buiquangtrung2012@gmail.com>
2026-04-15 09:08:32 +0700

    Automated Push for Standalone App

---
+++ public/mock_data.js
@@ -0,0 +1,30 @@
+/**
+ * MOCK DATA FOR HỒ SƠ DỰ ÁN
+ * Based on user's VBA table structure.
+ */
+
+export const MockData = {
+    nhanSu: [
+        ["1", "Nguyễn Văn Anh", "Chỉ huy trưởng", "Xây dựng cầu đường", "0971234567"],
+    ],
+    mayMoc: [
+        ["1", "Máy xúc đào Hitachi", "Xe", "01", "Công ty CP ABC", "Sở hữu"],
+        ["2", "Máy ủi Komatsu", "Xe", "02", "Công ty CP XYZ", "Đi thuê"],
+    ],
+    vatLieu: [
+        ["1", "Cát vàng", "TCVN", "Việt Trì, Phú Thọ", "Công ty cổ phần khai thác"],
+    ],
+    thiNghiem: [
+        ["1", "Công ty thí nghiệm", "xã AV, tỉnh BC", "LAS-XD 123  ", "Thí nghiệm xây dựng"],
+    ],
+    duAn: {
+        soHD: "127/HĐ-THTT ngày 18/03/2026",
+        tenDuAn: "Dự án đầu tư xây dựng hạ tầng kỹ thuật Khu đô thị mới Kim Chung - Di Trạch",
+        goiThau: "Gói thầu số 08: Thi công hệ thống thoát nước mưa và xử lý nước thải",
+        dvtc: "Công ty Cổ phần Tập đoàn Xây dựng Minh Hòa",
+        daiDienCDT: "Ban Quản lý dự án đầu tư xây dựng huyện Hoài Đức",
+        tvgs: "Công ty TNHH Tư vấn thiết kế và Giám sát xây dựng Thăng Long",
+        ngayKhoiCong: "01/01/2026",
+        ngayHoanThanh: "31/12/2026"
+    }
+};
